f2e0661e062cd47c1923ff0193132e61e6c66785
ishank dubey <ishankdubey@gmail.com>
2026-06-30 01:54:29 +0530

    Render Google tag in document head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import '../styles/globals.css';
@@ -6,18 +5,6 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-H4FFNWVWEM"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-H4FFNWVWEM');
-        `}
-      </Script>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,17 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H4FFNWVWEM" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H4FFNWVWEM');
+            `
+          }}
+        />
         <style>{'body{display:block!important}'}</style>
       </Head>
       <body>
