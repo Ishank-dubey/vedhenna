2502251621a93dc930d5d6b23c7b2b54fc54f1e5
ishank dubey <ishankdubey@gmail.com>
2026-06-30 01:12:52 +0530

    Add Google Analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import '../styles/globals.css';
@@ -5,6 +6,18 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-H4FFNWVWEM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-H4FFNWVWEM');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
